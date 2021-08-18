5bd1ef070e6d540ba8ca2e472f0455a29a0dc9f2
Taufik Pragusga <pragusga25@gmail.com>
2021-08-18 22:15:19 +0700

    fix build errors

--- src/components/Button/ContactMe.tsx
+++ src/components/Button/ContactMe.tsx
@@ -5,10 +5,10 @@ export const ContactMe = () => (
   <div>
     <button css={[contactMeBtnStyles, tw`text-lg w-52 h-14 lg:h-16 lg:text-2xl lg:w-56`]}>
       Contact Me
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span />
+      <span />
+      <span />
+      <span />
     </button>
   </div>
 );
